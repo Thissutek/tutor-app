32785e02d6826f5a781858eaa9f97c203575bb8f
unknown <jonathan_yau@outlook.com>
2025-02-27 15:19:25 -0500

    embedded Google Forms for the Contact page, easier to manage and free of use

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,16 @@
 export default function Contact() {
     return (
         <>
-            I'm the Contact Page
+            <div className="contact-form-container">
+                <h2>Contact Us</h2>
+                <iframe 
+                src="https://docs.google.com/forms/d/e/1FAIpQLSf9m_ORQ9FkL3Xn-zOA3ZmlU1JLX_YdXRrhwVj2YmqRo5AHTA/viewform?embedded=true" 
+                width="640" 
+                height="800" 
+                rameborder="0" 
+                marginheight="0" 
+                marginwidth="0">Loading…</iframe>
+            </div>
         </>
     )
 }
