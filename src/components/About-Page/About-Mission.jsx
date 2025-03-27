b492b9f0ae60c7ff5e40015d47abe77dfef68c4e
Jonathan Yau <jonathan-jcy@hotmail.com>
2025-03-27 11:33:48 -0400

    vercel file for routes

--- src/components/About-Page/About-Mission.jsx
+++ src/components/About-Page/About-Mission.jsx
@@ -5,7 +5,7 @@ export default function AboutMission() {
         <h2 className="text-3xl font-bold mb-4 header-style-inverse text-center">
           Our Mission
         </h2>
-        <p className="text-md leading-relaxed text-style-inverse sm:mb-10">
+        <p className="text-md leading-relaxed text-style-inverse">
           At Aims2Learn, we believe that great tutoring goes beyond just teaching—it’s about understanding each student’s unique learning needs in the moment. Our tutors are not only knowledgeable and experienced but also adaptable and intuitive, ensuring that every session is tailored to what the student needs most—whether it’s structured lessons, homework support, or independent problem-solving guidance. We don’t just give answers; we analyze, guide, and encourage critical thinking, helping students build confidence and long-term academic success. With patience, generosity, and a gentle approach, we create a supportive learning environment where students feel empowered to grow, learn, and excel at their own pace. At Aims2Learn, we’re not just tutors—we’re dedicated mentors invested in every student’s success.
         </p>
       </div>
